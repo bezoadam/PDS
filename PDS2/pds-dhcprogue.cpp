82e5af3a699b42e00c0f34fc158e7e396a846cb1
Adam Bezak <bezoadam95@gmail.com>
2018-03-16 11:11:48 +0100

    receiving dhcp discover

--- PDS2/pds-dhcprogue.cpp
+++ PDS2/pds-dhcprogue.cpp
@@ -3,41 +3,44 @@
  *	Author: Adam Bezak xbezak01
  */
 
-#include "pds-dhcpstarve.h"
+#include "pds-dhcprogue.h"
+
+volatile sig_atomic_t flag = 0;
 
 int main(int argc, char **argv) {
   
   	int ch;
  	bool isInterface, isPool, isGateway, isDns, isDomain, isLeasetime = false;
+ 	input_t *inputStruct = new input;
 	string pool, delimeter = "-";
 
 	while ((ch = getopt(argc, argv, "i:p:g:n:d:l:")) != -1) { // kontrola argumentov
 		switch (ch) {
 			case 'i' :
-				input.interface = optarg;
+				inputStruct->interface = optarg;
 				isInterface = true;
 				break;
 			case 'p' :
 				pool = optarg;
-				input.startPool = pool.substr(0, pool.find(delimeter));
-				input.endPool = pool.substr(pool.find(delimeter) + 1, pool.length());
+				inputStruct->startPool = pool.substr(0, pool.find(delimeter));
+				inputStruct->endPool = pool.substr(pool.find(delimeter) + 1, pool.length());
 				isPool = true;
 				break;
 			case 'g' :
-				input.gateway = optarg;
+				inputStruct->gateway = optarg;
 				isGateway = true;
 				break;
 			case 'n' :
-				input.dns = optarg;
+				inputStruct->dns = optarg;
 				isDns = true;
 				break;
 			case 'd':
-				input.domain = optarg;
+				inputStruct->domain = optarg;
 				isDomain = true;
 				break;
 			case 'l':
 				char *t;
-				input.leasetime = strtol(optarg, &t, 10);
+				inputStruct->leasetime = strtol(optarg, &t, 10);
 				isLeasetime = true;
 				if (*t != '\0') {
 					print_error(ERR_BADPARAMS);
@@ -56,7 +59,108 @@ int main(int argc, char **argv) {
 		exit(ERR_BADPARAMS);
 	}
 
+	/* Odchytenie SIG INT signalu */
+	struct sigaction sigIntHandler;
+
+	sigIntHandler.sa_handler = sigCatch;
+	sigemptyset(&sigIntHandler.sa_mask);
+	sigIntHandler.sa_flags = 0;
+
+	sigaction(SIGINT, &sigIntHandler, NULL);
+
+	dhcp_t *dhcpDiscover = new dhcp;
+	int socket;
+
+	configureSocket(&socket, inputStruct->interface);
+
+	*dhcpDiscover = waitForDiscover(&socket);
+
+	if ((close(socket)) == -1)      // close the socket
+	err(1,"close() failed");
+	printf("Closing socket ...\n");
     return 0;
+}
+
+dhcp_t waitForDiscover(int *socket) {
+	int n;
+	struct sockaddr_in   addrIn, addrOut;
+	socklen_t addrlen;
+
+	// memset(&addrIn,0,sizeof(addrIn));
+	// addrIn.sin_family=AF_INET;
+	// addrIn.sin_addr.s_addr=inet_addr("255.255.255.255");
+	// addrIn.sin_port=htons(67);
+	
+	dhcp_t *dhcpDiscover = new dhcp;
+	cout << "tutuddtt \n" << flush;
+		/* Citanie dat zo socketu */
+	while ((n= recvfrom(*socket, dhcpDiscover, sizeof(dhcp), 0, (struct sockaddr *) &addrIn, &addrlen)) >= 0) {
+		if (flag) {
+			cout << "data";
+			return *dhcpDiscover;
+		}
+	}
+
+	cout << "received \n" << flush;
+
+	if (dhcpDiscover->bp_options[2] == (uint8_t)DHCP_OPTION_DISCOVER) {
+		printf("DHCP DISCOVER received\n");
+		return *dhcpDiscover;
+	}
+}
+
+void configureSocket(int *sock, string interface) {
+	struct sockaddr_in   addrIn;   // Datova struktura adries
+	struct ifreq ifr;
+
+	int on = 1; // socket option
+
+	/* Vytvorenie socketu */
+	if ((*sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
+	err(1,"socket failed()");
+
+	/* I want to get an IPv4 IP address */
+ 	ifr.ifr_addr.sa_family = AF_INET;
+
+ 	/* I want IP address attached to "eth0" */
+ 	strncpy(ifr.ifr_name, interface.c_str(), IFNAMSIZ-1);
+
+ 	ioctl(*sock, SIOCGIFADDR, &ifr);
+
+ 	/* display result */
+	printf("%s\n", inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr));
+
+	memset(&addrIn,0,sizeof(addrIn));
+	addrIn.sin_family=AF_INET;
+	addrIn.sin_addr.s_addr=inet_addr("255.255.255.255");
+	addrIn.sin_port=htons(67);
+
+	/* Povolenie reusovania lokalnych adries na porte */
+	if ((setsockopt(*sock, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on))) == -1)
+	err(1,"setsockopt failead()");
+
+	/* Povolenie odosielania broadcastu socketom */
+	if ((setsockopt(*sock, SOL_SOCKET, SO_BROADCAST, &on, sizeof(on))) == -1)
+	err(1,"setsockopt failebd()");
+
+	/* Povolenie pozuivat viacero portov na jednom sockete */
+	if ((setsockopt(*sock, SOL_SOCKET, SO_REUSEPORT, &on, sizeof(on))) == -1)
+	err(1,"setsockopt failecd()");
+
+	/* Nabinovanie socketu na rozhranie */
+	if ((setsockopt(*sock, SOL_SOCKET, SO_BINDTODEVICE, interface.c_str(), interface.length())) == -1)
+	err(1,"setsockopt failedd()");
+
+	if ((::bind(*sock, (struct sockaddr *) &addrIn, sizeof(addrIn))) < 0) {
+		if ((close(*sock)) == -1)      // close the socket
+		err(1,"close() failed");
+		err(1,"fail");
+	}
+}
+
+void sigCatch(int sig) {
+	cout << "\nSIGINT CATCH\n";
+	flag = 1;
 }
 
 void print_error(int err) {
--- PDS2/pds-dhcprogue.h
+++ PDS2/pds-dhcprogue.h
@@ -1,6 +1,6 @@
 /**
- *	Program: PDS
- *	Author: Adam Bezak xbezak01
+ *  Program: PDS
+ *  Author: Adam Bezak xbezak01
  */
 
 #include <iostream>
@@ -9,32 +9,103 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <net/if.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <err.h>
+#include <signal.h>
+#include <inttypes.h>
+#include <iostream>
+#include <iomanip>
+#include <sys/ioctl.h>
 
 using namespace std;
 
+#define MESSAGE_TYPE_REQ_SUBNET_MASK        1
+#define MESSAGE_TYPE_ROUTER                 3
+#define MESSAGE_TYPE_DNS                    6
+#define MESSAGE_TYPE_DOMAIN_NAME            15
+#define MESSAGE_TYPE_REQ_IP                 50
+#define MESSAGE_TYPE_DHCP                   53
+#define MESSAGE_TYPE_PARAMETER_REQ_LIST     55
+#define MESSAGE_TYPE_END                    255
+
+#define DHCP_OPTION_DISCOVER               1
+#define DHCP_OPTION_OFFER                  2
+#define DHCP_OPTION_REQUEST                3
+#define DHCP_OPTION_ACK                    5
+
+#define DHCP_CHADDR_LEN  16
+#define DHCP_SNAME_LEN   64
+#define DHCP_FILE_LEN    128
+#define DHCP_OPTIONS_LEN 600
+
 enum {
-	NO_ERR = 0,		//0
-	ERR_BADPARAMS,	//1
+    NO_ERR = 0,     //0
+    ERR_BADPARAMS,  //1
 };
 
 const char *errors[] = {
-	"Ziadna chyba.",
-	"Chyba vo vstupnych parametroch"
+    "Ziadna chyba.",
+    "Chyba vo vstupnych parametroch"
 };
 
-struct input_t {
-	string interface;
-	string startPool;
-	string endPool;
-	string gateway;
-	string dns;
-	string domain;
-	int leasetime;
-} input;
+typedef struct input {
+    string interface;
+    string startPool;
+    string endPool;
+    string gateway;
+    string dns;
+    string domain;
+    int leasetime;
+} input_t;
+
+/**
+    Struktura DHCP packetu.
+*/
+typedef struct dhcp {
+    uint8_t    opcode;
+    uint8_t    htype;
+    uint8_t    hlen;
+    uint8_t    hops;
+    uint32_t   xid;
+    uint16_t   secs;
+    uint16_t   flags;
+    uint32_t   ciaddr;
+    uint32_t   yiaddr;
+    uint32_t   siaddr;
+    uint32_t   giaddr;
+    uint8_t    chaddr[DHCP_CHADDR_LEN];
+    uint8_t    bp_sname[DHCP_SNAME_LEN];
+    uint8_t    bp_file[DHCP_FILE_LEN];
+    uint32_t   magic_cookie;
+    uint8_t    bp_options[DHCP_OPTIONS_LEN];
+} dhcp_t;
+
+dhcp_t waitForDiscover(int *socket);
+
+/**
+    Handler pri odchyteni ukoncujuceho signalu.
+
+    @param sig Cislo signalu
+    @return void
+*/
+void sigCatch(int sig);
+
+/**
+    Konfiguracia socketu na odoslielanie a prijmanie Broadcast packetov na dane rozhrannie.
+
+    @param *sock Odkaz na socket
+    @param interface Interface zadany zo vstupnych argumentov
+    @return void
+*/
+void configureSocket(int *sock, string interface);
 
 /*
-*	Funkcia na vypis error statusov
-*	@param err cislo erroru
-*	@return void
+*   Funkcia na vypis error statusov
+*   @param err cislo erroru
+*   @return void
 */
 void print_error(int err);
